4367dd5dac494172bc080eb0fb8aac72a549597b
tmathew91 <97205675+tmathew91@users.noreply.github.com>
2022-12-01 14:27:51 +1100

    Add files via upload
    Began planning out Header component

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+
+export const Header = () => {
+  return (
+    <div className='container header__container'>
+    <h5>Hello I'm</h5>
+    <h1>Tom Mathew</h1>
+    <h5 className='text-light'> Fullstack Developer</h5>
+    </div>
+  )
+}
+
+export default Header
