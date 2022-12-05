4bdb38d04cc4659ac8cb0d70bffae4a6851ab721
tmathew91 <97205675+tmathew91@users.noreply.github.com>
2022-12-05 14:59:20 +1100

    Add files via upload
    add css styling to header component

--- Header.jsx
+++ Header.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/lqwpi-removebg-preview.png'
+import HeaderSocials from './HeaderSocials'
+
 
 export const Header = () => {
   return (
+    <header>
     <div className='container header__container'>
     <h5>Hello I'm</h5>
     <h1>Tom Mathew</h1>
     <h5 className='text-light'> Fullstack Developer</h5>
+    <CTA />
+    <HeaderSocials />
+
+    <div className='me'>
+      <img src={ME} alt="me"/>
     </div>
+
+    <a href='#contact' className='scroll__down'> Scroll Down</a>
+    </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
