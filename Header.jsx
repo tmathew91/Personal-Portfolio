4b5d61d5316f7c9f84a4e00f37689bc02a0e4942
tmathew91 <97205675+tmathew91@users.noreply.github.com>
2022-12-07 13:18:41 +1100

    Add files via upload
    finished header

--- Header.jsx
+++ Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/lqwpi-removebg-preview.png'
+import ME from '../../assets/meimagecropped.png'
 import HeaderSocials from './HeaderSocials'
 
 
@@ -9,17 +9,17 @@ export const Header = () => {
   return (
     <header>
     <div className='container header__container'>
-    <h5>Hello I'm</h5>
-    <h1>Tom Mathew</h1>
-    <h5 className='text-light'> Fullstack Developer</h5>
-    <CTA />
-    <HeaderSocials />
+      <h5>Hello I'm</h5>
+      <h1>Tom Mathew</h1>
+      <h5 className='text-light'> Fullstack Developer</h5>
+      <CTA />
+      <HeaderSocials />
 
-    <div className='me'>
-      <img src={ME} alt="me"/>
-    </div>
+      <div className='me'>
+        <img src={ME} alt="me"/>
+      </div>
 
-    <a href='#contact' className='scroll__down'> Scroll Down</a>
+      <a href='#contact' className='scroll__down'> Scroll Down</a>
     </div>
     </header>
   )
